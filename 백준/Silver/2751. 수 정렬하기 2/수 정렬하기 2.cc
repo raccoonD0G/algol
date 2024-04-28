0e4f10d5e3126a6c787bb340ef080d4c94af74cd
raccoonD0G <138660933+raccoonD0G@users.noreply.github.com>
2024-04-28 12:52:39 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 304 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,28 +1,54 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <algorithm> // std::swap 함수 사용을 위해 포함
 
 using namespace std;
 
+void QuickSort(vector<int>& arr, int left, int right) {
+    if (left >= right) return;
 
-int main()
-{
-	int n;
-	cin >> n;
+    int mid = left + (right - left) / 3;
+    swap(arr[left], arr[mid]);
+    int pivot = left;
 
-	vector<int> numVector;
+    int i = left + 1;
+    int j = right - 1;
+    while (true) {
+        for (; i < right; i++) {
+            if (arr[pivot] < arr[i]) break;
+        }
+        for (; j > left; j--) {
+            if (arr[pivot] > arr[j]) break;
+        }
+        if (i < j) {
+            swap(arr[i], arr[j]);
+        } else {
+            break;
+        }
+    }
+    swap(arr[pivot], arr[j]);
+    QuickSort(arr, left, j);
+    QuickSort(arr, j + 1, right);
+}
 
-	int tmp;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> tmp;
-		numVector.push_back(tmp);
-	}
+int main() {
+    ios::sync_with_stdio(false); // C++ 표준 스트림과 C 스타일 스트림의 동기화 비활성화
+    cin.tie(NULL); // cin과 cout의 tie를 끊음
 
-	sort(numVector.begin(), numVector.end());
+    int N;
+    cin >> N;
+    vector<int> numbers(N);
 
-	for (int i = 0; i < n; i++)
-	{
-		cout << numVector[i] << " ";
-	}
+    for (int i = 0; i < N; i++) {
+        cin >> numbers[i];
+    }
+
+    QuickSort(numbers, 0, N);
+
+    for (int number : numbers) {
+        cout << number << "\n";
+    }
+
+    cout.flush(); // 모든 출력을 확실히 보내기 위해 flush 호출
+    return 0;
 }
